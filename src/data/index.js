3615fe7483b786afb8c9b0d9134a58defb0f2294
shashank-iter <pandeyshashank812@gmail.com>
2024-08-16 20:33:39 +0530

    added data source

---
+++ src/data/index.js
@@ -0,0 +1,248 @@
+export const products = [
+  {
+    id: 1,
+    name: "Miyazaki Men Kurta",
+    href: "#",
+    price: "2999",
+    description:
+      "Premium Kurta with a touch of Miyazaki. Made from the finest cotton and designed for comfort.",
+    options: "8 colors",
+    quantity: 1,
+    images: [
+      {
+        id: 1,
+        src: "https://images.meesho.com/images/products/392657271/q57fc_512.webp",
+        alt: "Eight shirts arranged on table in black, olive, grey, blue, white, red, mustard, and green.",
+      },
+      {
+        id: 2,
+        src: "	https://images.meesho.com/images/products/392657272/wud0s_512.webp",
+        alt: "Front of plain black t-shirt.",
+      },
+    ],
+    details: [
+      {
+        name: "Size",
+        items: ["XS", "S", "M", "L", "XL"],
+      },
+    ],
+    imageSrc:
+      "https://images.meesho.com/images/products/392657271/q57fc_512.webp",
+    imageAlt:
+      "Eight shirts arranged on table in black, olive, grey, blue, white, red, mustard, and green.",
+  },
+  {
+    id: 2,
+    name: "Rajasthani Kurti",
+    href: "#",
+    price: "3999",
+    description:
+      "Minimalist Kurti with a touch of Miyazaki. Made from the finest cotton and designed for comfort.",
+    options: "Black",
+    quantity: 1,
+    images: [
+      {
+        id: 1,
+        src: "	https://images.meesho.com/images/products/415937167/oajww_512.webp",
+        alt: "Kurti with a touch of Miyazaki.",
+      },
+    ],
+    details: [
+      {
+        name: "Size",
+        items: ["XS", "S", "M", "L", "XL"],
+      },
+    ],
+    imageSrc:
+      "	https://images.meesho.com/images/products/415937167/oajww_512.webp",
+    imageAlt: "Front of plain black t-shirt.",
+  },
+  {
+    id: 3,
+    name: "Punjabi Kurta",
+    href: "#",
+    price: "6999",
+    description:
+      "Men's Kurta with a touch of Miyazaki. Made from the finest cotton and designed for comfort.",
+    options: "8 colors",
+    quantity: 1,
+    images: [
+      {
+        id: 1,
+        src: "https://images.meesho.com/images/products/279990214/zesg6_512.webp",
+        alt: "Men's Kurta with a touch of Miyazaki.",
+      },
+      {
+        id: 2,
+        src: "https://images.meesho.com/images/products/279990215/ddjbv_512.webp",
+        alt: "Men's Kurta with a touch of Miyazaki.",
+      },
+    ],
+    details: [
+      {
+        name: "Size",
+        items: ["XS", "S", "M", "L", "XL"],
+      },
+    ],
+    imageSrc:
+      "https://images.meesho.com/images/products/279990215/ddjbv_512.webp",
+    imageAlt: "Men's Kurta with a touch of Miyazaki.",
+  },
+  {
+    id: 4,
+    name: "Banarasi Saree",
+    href: "#",
+    price: "20999",
+    description:
+      "Banarasi Saree with a touch of Miyazaki. Made from the finest cotton and designed for comfort.",
+    options: "Black",
+    quantity: 1,
+    images: [
+      {
+        id: 1,
+        src: "https://images.meesho.com/images/products/413547105/dhpgq_512.webp",
+        alt: "Saree with a touch of Miyazaki.",
+      },
+      {
+        id: 2,
+        src: "https://images.meesho.com/images/products/413547105/fp3pk_512.webp",
+        alt: "Saree with a touch of Miyazaki.",
+      },
+    ],
+    details: [
+      {
+        name: "Size",
+        items: ["XS", "S", "M", "L", "XL"],
+      },
+    ],
+    imageSrc:
+      "https://images.meesho.com/images/products/413547105/dhpgq_512.webp",
+    imageAlt: "Front of plain black t-shirt.",
+  },
+  {
+    id: 5,
+    name: "Casual Kurta",
+    href: "#",
+    price: "1099",
+    description:
+      "Casual Kurta with a touch of Miyazaki. Made from the finest cotton and designed for comfort.",
+    options: "8 colors",
+    quantity: 1,
+    images: [
+      {
+        id: 1,
+        src: "https://images.meesho.com/images/products/389616397/cld6t_512.webp",
+        alt: "Casual Kurta with a touch of Miyazaki.",
+      },
+      {
+        id: 2,
+        src: "https://images.meesho.com/images/products/389616397/fxyc3_512.webp",
+        alt: "Front of plain black t-shirt.",
+      },
+    ],
+    details: [
+      {
+        name: "Size",
+        items: ["XS", "S", "M", "L", "XL"],
+      },
+    ],
+    imageSrc:
+      "	https://images.meesho.com/images/products/389616397/cld6t_512.webp",
+    imageAlt: "Casual Kurta with a touch of Miyazaki.",
+  },
+  {
+    id: 6,
+    name: "Festive Kurta",
+    href: "#",
+    price: "999",
+    description:
+      "Feel the festive vibes with this Kurta. Made from the finest cotton and designed for comfort.",
+    options: "Black",
+    quantity: 1,
+    images: [
+      {
+        id: 1,
+        src: "https://images.meesho.com/images/products/299935503/moyzk_64.webp",
+        alt: "Feel the festive vibes with this Kurta.",
+      },
+      {
+        id: 2,
+        src: "https://images.meesho.com/images/products/299935503/aeojh_512.webp",
+        alt: "Feel the festive vibes with this Kurta.",
+      },
+    ],
+    details: [
+      {
+        name: "Size",
+        items: ["XS", "S", "M", "L", "XL"],
+      },
+    ],
+    imageSrc:
+      "https://images.meesho.com/images/products/299935503/moyzk_64.webp",
+    imageAlt: "Festive Kurta with a touch of Miyazaki.",
+  },
+  {
+    id: 7,
+    name: "Georgette Saree",
+    href: "#",
+    price: "7999",
+    description:
+      "Gorgette Saree with a touch of Miyazaki. Made from the finest cotton and designed for comfort.",
+    options: "8 colors",
+    quantity: 1,
+    images: [
+      {
+        id: 1,
+        src: "	https://images.meesho.com/images/products/325100981/uokgq_512.webp",
+        alt: "Georgette Saree with a touch of Miyazaki.",
+      },
+      {
+        id: 2,
+        src: "	https://images.meesho.com/images/products/325100981/dcrk6_512.webp",
+        alt: "Georgette Saree with a touch of Miyazaki.",
+      },
+    ],
+    details: [
+      {
+        name: "Size",
+        items: ["XS", "S", "M", "L", "XL"],
+      },
+    ],
+    imageSrc:
+      "	https://images.meesho.com/images/products/325100981/uokgq_512.webp",
+    imageAlt: "Georgette Saree with a touch of Miyazaki.",
+  },
+  {
+    id: 8,
+    name: "Men Ethinic Jacket",
+    href: "#",
+    price: "5999",
+    description:
+      "Men's Jacket with a touch of Miyazaki. Made from the finest cotton and designed for comfort.",
+    options: "Black",
+    quantity: 1,
+    images: [
+      {
+        id: 1,
+        src: "https://images.meesho.com/images/products/275904106/w1vqb_512.webp",
+        alt: "Men's Jacket with a touch of Miyazaki.",
+      },
+      {
+        id: 2,
+        src: "https://images.meesho.com/images/products/275904106/2fbm2_512.webp",
+        alt: "Men's Jacket with a touch of Miyazaki.",
+      },
+    ],
+    details: [
+      {
+        name: "Size",
+        items: ["XS", "S", "M", "L", "XL"],
+      },
+    ],
+    imageSrc:
+      "https://images.meesho.com/images/products/275904106/w1vqb_512.webp",
+    imageAlt: "Men's Jacket with a touch of Miyazaki.",
+  },
+
+  // More products...
+];
