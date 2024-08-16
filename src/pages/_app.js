497f659ab33510ff99f44cbeaf4189da32cb637b
shashank-iter <pandeyshashank812@gmail.com>
2024-08-16 20:34:35 +0530

    added checkout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
-
+import dynamic from "next/dynamic";
+import { Toaster } from "react-hot-toast";
+const Layout = dynamic(() => import("@/components/Layout/Layout"), {
+  ssr: false,
+});
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Toaster />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
