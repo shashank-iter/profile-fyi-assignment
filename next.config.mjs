2831aed957a0ef9790b590251a82c488d013d1f5
shashank-iter <pandeyshashank812@gmail.com>
2024-08-16 20:31:39 +0530

    configuration and inital commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["tailwindui.com", "images.meesho.com"], // Allow images from tailwindui.com
+  },
 };
 
 export default nextConfig;
